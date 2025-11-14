34c4e7b4c090c21196da5b5816927a914650d50e
huuyii <3134846106@qq.com>
2025-11-14 22:01:16 +0800

    柯里化函数

---
+++ 15.函数柯里化.js
@@ -0,0 +1,28 @@
+/**
+ * 函数柯里化（Currying）是一种在 JavaScript 中处理函数的技术，
+ * 它可以将一个接受多个参数的函数转换成一系列使用一个参数的函数。
+ * 这个转换后的函数链中的每一个函数都返回下一个函数，直到最后一个函数返回最终的结果
+ */
+
+function curry(fn) {
+  if (fn.length === 0) {
+    return fn;
+  }
+  function _curried(depth, args) {
+    return function (newArgument) {
+      if (depth - 1 === 0) {
+        return fn(...args, newArgument);
+      }
+      return _curried(depth - 1, [...args, newArgument]);
+    };
+  }
+  return _curried(fn.length, [])
+}
+
+function add(a,b) {
+    return a + b
+}
+
+const curriedAdd = curry(add)
+const addFive = curriedAdd(5)
+console.log([1,2,3].map(addFive))
